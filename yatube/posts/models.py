766bda512f1cb04dade8963e9499d716f7c3b896
Егор Агарков <agaregor46@icloud.com>
2022-11-08 02:16:32 +0300

    Доработать формы по паролям

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -20,6 +20,9 @@ class Post(models.Model):
         related_name='posts'
     )
 
+    def __str__(self):
+        return self.text
+
 class Group(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(unique = True)
