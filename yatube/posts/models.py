ba59ec4cba7d48af4c6a6f224b6dfc1c48c3d520
Егор Агарков <agaregor46@icloud.com>
2022-11-10 14:35:09 +0300

    готова навигация

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,16 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+
+class Group(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique = True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -25,14 +35,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.text
-
-class Group(models.Model):
-    title = models.CharField(max_length=50)
-    slug = models.SlugField(unique = True)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.title
 
 
 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,25 +2,26 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group, User
 
-def paginator
+def paginator_func(post_list,request):
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    return paginator.get_page(page_number)
+
 
 def index(request):
     template = 'posts/index.html'
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     context = {
-        'page_obj': page_obj
+        'page_obj': paginator_func(post_list,request=request)
     }
     return render(request, template, context)
 def group_posts(request,slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()
     context = {
         'group': group,
-        'posts': posts,
+        'page_obj': paginator_func(post_list=posts,request=request)
     }
     return render(request, template, context)
 
@@ -28,19 +29,20 @@ def profile(request, username):
     template_name = 'posts/profile.html'
     author = get_object_or_404(User, username=username)
     posts = author.posts.all()
-    paginator = Paginator(posts, 10)
     context = {
-        'posts': posts,
+        'page_obj': paginator_func(post_list=posts,request=request),
         'author': author
     }
     return render(request, template_name, context)
 
 
 def post_detail(request, post_id):
-    # Здесь код запроса к модели и создание словаря контекста
+    template_name = 'posts/post_detail.html'
+    post = get_object_or_404(Post, pk=post_id)
     context = {
+        'post': post
     }
-    return render(request, 'posts/post_detail.html', context)
+    return render(request, template_name, context)
 
 
 # Create your views here.
